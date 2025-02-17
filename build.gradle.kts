80e315be43447de60279d7caeded7d3b57eeff77
人間工作 <coolxiaom95@gmail.com>
2025-02-17 23:55:40 +0800

    initial project

---
+++ build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    java
+}
+
+group = "top.mrxiaom"
+version = "1.0.2"
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven("https://oss.sonatype.org/content/groups/public/")
+    maven("https://jitpack.io")
+    maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
+}
+
+@Suppress("VulnerableLibrariesLocal")
+dependencies {
+    compileOnly("com.destroystokyo.paper:paper-api:1.16.5-R0.1-SNAPSHOT")
+    implementation("org.jetbrains:annotations:24.0.0")
+}
+
+val targetJavaVersion = 8
+java {
+    val javaVersion = JavaVersion.toVersion(targetJavaVersion)
+    sourceCompatibility = javaVersion
+    targetCompatibility = javaVersion
+    if (JavaVersion.current() < javaVersion) {
+        toolchain.languageVersion.set(JavaLanguageVersion.of(targetJavaVersion))
+    }
+}
+
+tasks {
+    withType<JavaCompile>().configureEach {
+        if (targetJavaVersion >= 10 || JavaVersion.current().isJava10Compatible) {
+            options.release.set(targetJavaVersion)
+        }
+    }
+
+    jar {
+        destinationDirectory.set(rootProject.file("out"))
+    }
+
+    processResources {
+        duplicatesStrategy = DuplicatesStrategy.INCLUDE
+        from(sourceSets.main.get().resources.srcDirs) {
+            expand("version" to project.version)
+            include("plugin.yml")
+        }
+    }
+}
